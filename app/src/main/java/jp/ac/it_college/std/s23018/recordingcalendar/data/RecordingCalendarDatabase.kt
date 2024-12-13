a773ed825dd3f079d8107e0e71bc26fa79cb9658
itc-s23018 <s23018@std.it-college.ac.jp>
2024-12-13 09:55:01 +0900

    データベースを作成

---
+++ app/src/main/java/jp/ac/it_college/std/s23018/recordingcalendar/data/RecordingCalendarDatabase.kt
@@ -0,0 +1,37 @@
+package jp.ac.it_college.std.s23018.recordingcalendar.data
+
+import android.content.Context
+import androidx.compose.ui.Modifier
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import jp.ac.it_college.std.s23018.recordingcalendar.data.dao.RecordDao
+import jp.ac.it_college.std.s23018.recordingcalendar.data.dao.UserDao
+import jp.ac.it_college.std.s23018.recordingcalendar.data.entity.MotionEntity
+import jp.ac.it_college.std.s23018.recordingcalendar.data.entity.UserEntity
+import jp.ac.it_college.std.s23018.recordingcalendar.data.entity.WeightEntity
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.synchronized
+
+@Database(entities = [UserEntity::class, WeightEntity::class, MotionEntity::class],
+    version = 1,
+    exportSchema = false)
+abstract class RecordingCalendarDatabase: RoomDatabase() {
+    abstract fun userDao(): UserDao
+    abstract fun recordDao(): RecordDao
+
+    companion object{
+        @Volatile
+        private var Instance: RecordingCalendarDatabase? = null
+
+        @OptIn(InternalCoroutinesApi::class)
+        fun getDatabase(context: Context): RecordingCalendarDatabase {
+            return Instance ?: synchronized(this){
+                Room.databaseBuilder(
+                    context, RecordingCalendarDatabase::class.java,
+                    "recordCalendar_database"
+                ).build().also { Instance = it }
+            }
+        }
+    }
+}
